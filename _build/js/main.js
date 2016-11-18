98703b49120094a1b53b2fa4fef2a4b78701b38b
Nathan McElwain <nathanmcelwain@ymail.com>
2016-11-18 15:26:59 -0800

    final update

--- _build/js/main.js
+++ _build/js/main.js
@@ -1,57 +1,62 @@
 $(document).ready(function($) {
   var clientKey = 'Vg4h8km8IR34NENUfGKQLHFusV8N3WkawLlQOqW8XVovuqD7JY';
   var namedUrl = 'https://api.tumblr.com/v2/blog/';
-  var tagged = 'http://api.tumblr.com/v2/tagged?tag='
-  var myArr = [];
+  var tagged = 'http://api.tumblr.com/v2/tagged?tag=';
+  var htmlStart = '<li class="items"><h3>';
+  var bridge = '</h3><div class="each-item">';
+  var htmlEnd = '</div><input type="button" class="btn btn-default item-button" value="Add" /></li>';
 
-  var getBlog = function(){
+  var getBlog = function() {
     var query = $('#blog-title').val();
     $.ajax({
       url: namedUrl + query + '.tumblr.com/posts/text',
       type: 'GET',
       dataType: 'jsonp',
-      data: ({ api_key: clientKey})
+      data: ({api_key: clientKey})
     })
     .done(function(info) {
-      for(var i = 0; i<info.response.posts.length; i++){
+      for (var i = 0; i<info.response.posts.length; i++) {
         var items = info.response.posts[i];
-        if(items !== undefined){
-          $('#list').append('<li class="items"><div class="each-item"><h3>' + items.blog_name + '</h3>' + items.body + '</div><input type="button" class="btn btn-default item-button" value="Add" /></li>')
+        if (items !== undefined) {
+          $('#list').append(htmlStart + items.blog_name + bridge + '<h2>' + items.title + '</h2>' + items.body + htmlEnd);
         }
       }
     })
     .fail(function() {
       alert("Sorry, I couldn't find that, please check your search");
     });
+    $('#blog-title').val('');
   };
 
-  var getTags = function(){
+  var getTags = function() {
     var query = $('#tags').val();
     $.ajax({
       url: tagged + query,
       type: 'GET',
       dataType: 'jsonp',
-      data: ({ api_key: clientKey})
+      data: ({api_key: clientKey})
     })
     .done(function(info) {
-      for(var i = 0; i<info.response.length; i++){
+      for (var i = 0; i<info.response.length; i++) {
         var items = info.response[i];
-        if(items !== undefined){
-          if(items.type == 'photo'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><img src="'+ items.photos[0].original_size.url + '" width="400" height="400"></img></div><button class="btn btn-default item-button">Save</button></li>');
+        if (items !== undefined) {
+          if (items.type == 'photo') {
+            $('#list').append(htmlStart + items.summary + bridge + '<img src="'+ items.photos[0].original_size.url + '" width="400" height="400"></img>' + htmlEnd);
           }
           else if (items.type == 'quote'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><p><em>"..' + items.text + '.."</em></p></div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<p><em>"..' + items.text + '.."</em></p>' + htmlEnd);
           }
           else if (items.type == 'video'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><video src=">' + items.post_url + '" alt"cannot retrieve video"></video></div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<video src="' + items.post_url + '" alt="cannot retrieve video"></video>' + htmlEnd);
           }
           else if (items.type == 'answer'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><div>' + items.text + '</div</div><button class="btn btn-default item-button">Save</button></li>');
-          }else if (items.type == 'link'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><a href="' + items.url + '" target="_blank"></div</div><button class="btn btn-default item-button">Save</button></li>');
-          }else{
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><iframe width="500" height="400" src="' + items.post_url + '"></div</div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<div>' + items.text + '</div>' + htmlEnd);
+          }
+          else if (items.type == 'link'){
+            $('#list').append(htmlStart + items.summary + bridge + '<a href="' + items.url + '" target="_blank">' + htmlEnd);
+          }
+          else{
+            $('#list').append(htmlStart + items.summary + bridge + '<iframe width="500" height="400" src="' + items.post_url + '" alt="cannot retrieve this item, sorry"></iframe>' + htmlEnd);
           }
         }
       }
@@ -59,36 +64,39 @@ $(document).ready(function($) {
     .fail(function() {
       alert("Sorry, I couldn't find that, please check your search");
     });
+    $('#tags').val('');
   };
 
-  var getTaggedBlog = function(){
+  var getTaggedBlog = function() {
     var blogName = $('#blog-title').val();
     var tagged = $('#tags').val();
     $.ajax({
       url: namedUrl + blogName + '.tumblr.com/posts/?tag=' + tagged,
       type: 'GET',
       dataType: 'jsonp',
-      data: ({ api_key: clientKey})
+      data: ({api_key: clientKey})
     })
     .done(function(info) {
-      for(var i = 0; i<info.response.posts.length; i++){
+      for (var i = 0; i<info.response.posts.length; i++) {
         var items = info.response.posts[i];
-        if(items !== undefined){
-          if(items.type == 'photo'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><img src="'+ items.photos[0].original_size.url + '" width="400" height="400"></img></div><button class="btn btn-default item-button">Save</button></li>');
+        if (items !== undefined) {
+          if (items.type == 'photo') {
+            $('#list').append(htmlStart + items.summary + bridge + '<img src="'+ items.photos[0].original_size.url + '" width="400" height="400"></img>' + htmlEnd);
           }
           else if (items.type == 'quote'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><p><em>"..' + items.text + '.."</em></p></div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<p><em>"..' + items.text + '.."</em></p>' + htmlEnd);
           }
           else if (items.type == 'video'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><video src=">' + items.post_url + '" alt"cannot retrieve video"></video></div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<video src="' + items.post_url + '" alt="cannot retrieve video"></video>' + htmlEnd);
           }
           else if (items.type == 'answer'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><div>' + items.text + '</div</div><button class="btn btn-default item-button">Save</button></li>');
-          }else if (items.type == 'link'){
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><a href="' + items.url + '" target="_blank"></div</div><button class="btn btn-default item-button">Save</button></li>');
-          }else{
-            $('#list').append('<li class="items"><h3>' + items.summary + '</h3><div class="each-item"><iframe width="500" height="400" src="' + items.post_url + '"></div</div><button class="btn btn-default item-button">Save</button></li>');
+            $('#list').append(htmlStart + items.summary + bridge + '<div>' + items.text + '</div>' + htmlEnd);
+          }
+          else if (items.type == 'link'){
+            $('#list').append(htmlStart + items.summary + bridge + '<a href="' + items.url + '" target="_blank">' + htmlEnd);
+          }
+          else{
+            $('#list').append(htmlStart + items.summary + bridge + '<iframe width="500" height="400" src="' + items.post_url + '" alt="cannot retrieve this item, sorry"></iframe>' + htmlEnd);
           }
         }
       }
@@ -96,33 +104,53 @@ $(document).ready(function($) {
     .fail(function() {
       alert("Sorry, I couldn't find that, please check your search");
     });
+    $('#blog-title').val('');
+    $('#tags').val('');
   };
 
 
-  $('#go').on('click', function(){
+  $('#go').on('click', function() {
     $('#list').html('');
-    if($('#blog-title').val().trim() === '' && $('#tags').val().trim() === ''){
+    if ($('#blog-title').val().trim() === '' && $('#tags').val().trim() === '') {
       alert("I need there to be something to search for!")
     }
-    else if($('#tags').val().trim() === ''){
+    else if ($('#tags').val().trim() === '') {
       getBlog();
     }
-    else if($('#blog-title').val().trim() === ''){
+    else if ($('#blog-title').val().trim() === '') {
       getTags();
-    }else{
+    }
+    else {
       getTaggedBlog();
+    }
+  });
+
+  $(document).keypress(function(event) {
+    if (event.which === 13) {
+      $('#list').html('');
+      if ($('#blog-title').val().trim() === '' && $('#tags').val().trim() === '') {
+        alert("I need there to be something to search for!")
+      }
+      else if ($('#tags').val().trim() === '') {
+        getBlog();
+      }
+      else if ($('#blog-title').val().trim() === '') {
+        getTags();
+      }
+      else {
+        getTaggedBlog();
+      }
     }
   });
 
 
   $('#list').on('click', 'input[type=button]', function(event) {
     event.preventDefault();
-    $(this).parent().clone().appendTo('#fave-list');
-    $('#fave-list input[type=button]').toggleClass('remove-button').removeClass('item-button').attr('value', 'Remove');
-    $('#fave-list li').toggleClass('fave-items');
+    $(this).parent().clone().appendTo('#fave-list').removeClass('items').addClass('fave-items');
+    $('#fave-list input[type=button]').attr('value', 'Remove');
   });
 
-  $('#fave-list').on('click', 'input[type=button]', function(event){
+  $('#fave-list').on('click', 'input[type=button]', function(event) {
     event.preventDefault();
     $(this).parent().remove();
   });
